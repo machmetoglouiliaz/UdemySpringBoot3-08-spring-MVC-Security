565ef29f5ab2240570ea5becbd284ebc8cd6807c
Mourat Achmet oglou Iliaz <mayrit91@gmail.com>
2025-05-31 20:38:35 +0300

    Added JDBC authentication with BCrypt encryption and custom tables

--- 00-spring-boot-spring-security/src/main/java/com/mourat/udemy/springsecuritydemo/configuration/DemoSecurityConfig.java
+++ 00-spring-boot-spring-security/src/main/java/com/mourat/udemy/springsecuritydemo/configuration/DemoSecurityConfig.java
@@ -6,11 +6,15 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
-
+/*
     @Bean
     public InMemoryUserDetailsManager manager(){
         UserDetails john = User.builder()
@@ -32,6 +36,19 @@ public class DemoSecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+    
+ */
+
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+
+        JdbcUserDetailsManager manager = new JdbcUserDetailsManager(dataSource);
+
+        manager.setUsersByUsernameQuery("SELECT user_id, pw, active FROM members WHERE user_id=?");
+        manager.setAuthoritiesByUsernameQuery("SELECT user_id, role FROM roles WHERE user_id=?");
+
+        return manager;
     }
 
     @Bean
